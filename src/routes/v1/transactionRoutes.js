d6fd0464f7ffd8490d90b5ab6fdb5e3fd39f9148
Manish Singh <100526847+manish5476@users.noreply.github.com>
2026-02-23 09:55:49 +0530

    Update transactionRoutes.js

--- src/routes/v1/transactionRoutes.js
+++ src/routes/v1/transactionRoutes.js
@@ -6,29 +6,14 @@ const authController = require('../../modules/auth/core/auth.controller');
 const { checkPermission } = require("../../core/middleware/permission.middleware");
 const { PERMISSIONS } = require("../../config/permissions");
 
+// Protect all routes
 router.use(authController.protect);
 
+// Standard Transaction Routes
 router.get('/', checkPermission(PERMISSIONS.TRANSACTION.READ), transactionController.getTransactions);
 router.get('/export', checkPermission(PERMISSIONS.TRANSACTION.READ), transactionController.exportTransactionsCsv);
 
-// Monitor is Executive/Analytics
+// Monitor / Executive Dashboard Routes
 router.get("/monitor/transactions", checkPermission(PERMISSIONS.ANALYTICS.VIEW_EXECUTIVE), monitorController.getRecentTransactions);
 
 module.exports = router;
-// const express = require('express');
-// const router = express.Router();
-// const transactionController = require('../../modules/accounting/core/transaction.controller');
-// const monitorController = require('../../modules/_legacy/controllers/monitorController');
-// const authController = require('../../modules/auth/core/auth.controller');
-// const { checkPermission } = require("../../core/middleware/permission.middleware");
-// const { PERMISSIONS } = require("../../config/permissions");
-
-// router.use(authController.protect);
-
-// router.get('/', checkPermission(PERMISSIONS.TRANSACTION.READ), transactionController.getTransactions);
-// router.get('/export', checkPermission(PERMISSIONS.TRANSACTION.READ), transactionController.exportTransactionsCsv);
-
-// // Monitor is Executive/Analytics
-// router.get("/monitor/transactions", checkPermission(PERMISSIONS.ANALYTICS.VIEW_EXECUTIVE), monitorController.getRecentTransactions);
-
-// module.exports = router;
