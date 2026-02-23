88d0c867c9d03c6ca62735ea4cf2a5b071eabab2
Manish Singh <100526847+manish5476@users.noreply.github.com>
2026-02-23 10:09:37 +0530

    Update salesReturnRoutes.js

--- src/routes/v1/salesReturnRoutes.js
+++ src/routes/v1/salesReturnRoutes.js
@@ -3,8 +3,24 @@ const express = require('express');
 const router = express.Router();
 const salesReturnController = require('../../modules/inventory/core/salesReturn.controller');
 const authController = require('../../modules/auth/core/auth.controller');
+const { checkPermission } = require("../../core/middleware/permission.middleware");
+const { PERMISSIONS } = require("../../config/permissions");
+
+// Protect all routes
 router.use(authController.protect);
-router.post('/', salesReturnController.createReturn);
-router.get('/', salesReturnController.getReturns);
+
+// Create a new sales return
+router.post(
+  '/', 
+  checkPermission(PERMISSIONS.SALES_RETURN.MANAGE), 
+  salesReturnController.createReturn
+);
+
+// Get a list of sales returns
+router.get(
+  '/', 
+  checkPermission(PERMISSIONS.SALES_RETURN.READ), 
+  salesReturnController.getReturns
+);
 
 module.exports = router;
